9f616ee3d35e039a112f2c93f8503a8b89eabc7c
Dharshan <dharshan280706@gmail.com>
2025-04-18 19:19:43 +0530

    Corrected Files

--- Project/database.js
+++ Project/database.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mysql = require("mysql2");
 
 const dbConfig = {
@@ -23,30 +23,4 @@ pool.getConnection((err, connection) => {
     }
 });
 
-=======
-const mysql = require("mysql2");
-
-const dbConfig = {
-    host: "127.0.0.1",
-    port: 3306,
-    user: "dbmsuser",
-    password: "dbmsproject",
-    database: "dbms_project",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-};
-
-const pool = mysql.createPool(dbConfig);
-
-pool.getConnection((err, connection) => {
-    if (err) {
-        console.error("❌ Database Pool Creation Error or Initial Connection Failed:", err.message);
-    } else {
-        console.log("✅ Database Pool created & initial connection successful!");
-        connection.release();
-    }
-});
-
->>>>>>> 5cbaa376ebf85c7b9ba5c4342d2116207091147c
 module.exports = pool;
